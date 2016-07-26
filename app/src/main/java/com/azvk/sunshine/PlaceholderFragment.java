fa4647dfc581add07bd228b2a8f32876d59bd008
AlexRedchets <alexey.redchets@gmail.com>
2016-07-26 17:17:47 -0400

    ListView + Adapter

--- app/src/main/java/com/azvk/sunshine/PlaceholderFragment.java
+++ app/src/main/java/com/azvk/sunshine/PlaceholderFragment.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
 
@@ -15,6 +21,43 @@ public class PlaceholderFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Initialize view
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        //Initialize adapter for list view
+        ArrayAdapter<String> adapter;
+
+        //String array for list view
+        String[] array = {"Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+        };
+
+        //Convert string array into ArrayList
+        List<String> arrayList = new ArrayList<>(Arrays.asList(array));
+
+        //Initialize adapter
+        adapter = new ArrayAdapter<>(
+                //Current view
+                getActivity(),
+                //Layout list item
+                R.layout.list_item_forecast,
+                //TextView
+                R.id.list_item_forecast_textview,
+                arrayList);
+
+        //Initialize List View
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        //Set adapter for List View
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
