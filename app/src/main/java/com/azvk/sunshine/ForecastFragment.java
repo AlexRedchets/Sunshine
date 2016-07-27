86baeec9b02936f4050d8ef386ba0ceb0dc1d7a3
AlexRedchets <alexey.redchets@gmail.com>
2016-07-27 18:10:51 -0400

    Shared Preferences were added to save location info

--- app/src/main/java/com/azvk/sunshine/ForecastFragment.java
+++ app/src/main/java/com/azvk/sunshine/ForecastFragment.java
@@ -1,9 +1,12 @@
 package com.azvk.sunshine;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
@@ -38,6 +41,8 @@ public class ForecastFragment extends Fragment {
 
     //Initialize adapter for list view
     ArrayAdapter<String> adapter;
+
+    String location;
 
     public ForecastFragment() {
     }
@@ -328,7 +333,10 @@ public class ForecastFragment extends Fragment {
                 return true;
             case R.id.action_refresh:
                 FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-                fetchWeatherTask.execute("94043");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String location = prefs.getString(getString(R.string.pref_location_key),
+                        getString(R.string.pref_location_default));
+                fetchWeatherTask.execute(location);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
