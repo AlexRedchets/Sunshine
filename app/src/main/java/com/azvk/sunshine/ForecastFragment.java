6651342c82a35a7eacd42b1fddda069cdab44d82
AlexRedchets <alexey.redchets@gmail.com>
2016-07-27 09:02:31 -0400

    AsyncTask added

---
+++ app/src/main/java/com/azvk/sunshine/ForecastFragment.java
@@ -0,0 +1,143 @@
+package com.azvk.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //Initialize view
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        //Initialize adapter for list view
+        ArrayAdapter<String> adapter;
+
+        //String array for list view
+        String[] array = {"Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+                "Today - Sunny - 88/63",
+        };
+
+        //Convert string array into ArrayList
+        List<String> arrayList = new ArrayList<>(Arrays.asList(array));
+
+        //Initialize adapter
+        adapter = new ArrayAdapter<>(
+                //Current view
+                getActivity(),
+                //Layout list item
+                R.layout.list_item_forecast,
+                //TextView
+                R.id.list_item_forecast_textview,
+                arrayList);
+
+        //Initialize List View
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        //Set adapter for List View
+        listView.setAdapter(adapter);
+
+        return view;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
+
+        public final String TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/azvk/sunshine/MainActivity.java
+++ app/src/main/java/com/azvk/sunshine/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.activity_main, new PlaceholderFragment())
+                    .add(R.id.activity_main, new ForecastFragment())
                     .commit();
         }
     }
