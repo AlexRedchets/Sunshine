eebca71cecef9050e9b62ccb9c46a63e32a1cc29
AlexRedchets <alexey.redchets@gmail.com>
2016-07-27 15:41:04 -0400

    DetailActivity with DetailFragment created

--- app/src/main/java/com/azvk/sunshine/ForecastFragment.java
+++ app/src/main/java/com/azvk/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.azvk.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,8 +14,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -47,7 +50,7 @@ public class ForecastFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         //Initialize view
         View view = inflater.inflate(R.layout.fragment_main, container, false);
@@ -85,7 +88,15 @@ public class ForecastFragment extends Fragment {
         listView = (ListView) view.findViewById(R.id.listview_forecast);
         //Set adapter for List View
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), "You pushed " + adapter.getItem(i), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), DetailActivity.class);
+                startActivity(intent);
 
+            }
+        });
         return view;
     }
 
